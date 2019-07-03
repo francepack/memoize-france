d371c52e2864963811395c3c6a69c385bd0a71cf
Mason France <masonrf1@gmail.com>
2019-07-03 17:34:55 -0600

    Revise category review display

--- src/Finish/Finish.js
+++ src/Finish/Finish.js
@@ -3,8 +3,10 @@ import React from 'react';
 export function Finish(props) {
   return(
     <div className='finish-category-summary'>
-      <p className='category-finish'>All questions attempted.</p>
-      <p>You answered {props.correctCount} of {props.questionLength} correctly.</p>
+      <div className='summary'>
+        <p className='category-finish'>All questions attempted.</p>
+        <p>You answered {props.correctCount} of {props.questionLength} correctly.</p>
+      </div>
       <button onClick={props.refreshCategory} className='refresh-category'>Restart Category</button>
     </div>
   );
